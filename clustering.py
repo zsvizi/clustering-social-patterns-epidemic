c9dd9280bff327280624cdba1d0a812fccc3a142
Evans-Korir <evanskorir6@gmail.com>
2022-04-22 15:41:32 +0200

    -implemented euclidean distance for the KMeans clustering.py -created distance matrix -sort the distance matrix using a hierarchical clustering method

--- clustering.py
+++ clustering.py
@@ -1,9 +1,10 @@
 import numpy as np
-from scipy.spatial.distance import pdist, squareform
-from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
-import math
+from scipy.spatial.distance import cdist
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist, squareform
+from fastcluster import linkage
 
 from dataloader import DataLoader
 from plotter import Plotter
@@ -76,6 +77,7 @@ class Clustering:
         self.k_means_pred = k_means.predict(self.data)
         self.centroids = k_means.cluster_centers_
 
+
     def get_closest_points(self):
         self.closest_point_idx = (-1) * np.ones(self.n_cl).astype(int)
         for c_idx, centroid in enumerate(self.centroids):
@@ -86,32 +88,78 @@ class Clustering:
                     if min_dist is None or dist < min_dist:
                         min_dist = dist
                         self.closest_point_idx[c_idx] = idx
-        self.closest_points = self.data[np.array(self.closest_point_idx).astype(int), :2]
+                        self.closest_points = self.data[np.array(self.closest_point_idx).astype(int), :2]
 
-    def euclidean_distance_for_clustering(self):
-        self.data = DataLoader
-        data_tr = DataTransformer()
-        size = len(data_tr.data_clustering)  # we have a square matrix
-        result = 0.0
-        for i in range(size):
-            for j in range(size):
-                d1 = float(data_tr.data_clustering[i])
-                d2 = float(data_tr.data_clustering[j])
-                dev = d1 - d2
-                result += pow(dev, 2)
-            result = math.sqrt(result)
-            return result
+    def get_distance_points(self, no_of_iterations):
+        idx = np.random.choice(len(self.data), self.n_cl, replace=False)  # Randomly choosing Centroids
+        centroids = self.data[idx, :]
+        distances = cdist(self.data, centroids, 'euclidean')
+        points = np.array([np.argmin(i) for i in distances])
+        for _ in range(no_of_iterations):
+            centroids = []
+            for idx in range(self.n_cl):
+                temp_cent = self.data[points == idx].mean(axis=0)
+                centroids.append(temp_cent)
+                centroids = np.vstack(centroids)
+                distances = cdist(self.data, centroids, 'euclidean')
+                points = np.array([np.argmin(i) for i in distances])
+                return points
 
-    def get_pairwise_distance_for_clustering(self):
-        dist_matrix = []
-        self.data = DataLoader
-        data_tr = DataTransformer()
-        data_tr.data_clustering_size = len(data_tr.data_clustering)
-        for i in range(data_tr.data_clustering_size):
-            for j in range(data_tr.data_clustering_size):
-                dist = squareform(pdist(data_tr.data_clustering[i], data_tr.data_clustering[j]))
-                dist_matrix.append(dist)
-        return dist_matrix
+    def distance(self):
+        dist_matrix = np.zeros(16, 16)
+        length = self.data.shape
+        for i in range(length[0]):
+            for j in range(length[1]):
+                dist_matrix = squareform(pdist(self.data))
+        plt.imshow(dist_matrix)
+
+
+def seriation(Z, N, cur_index):
+    '''
+        input:
+            - Z is a hierarchical tree (dendrogram)
+            - N is the number of points given to the clustering process
+            - cur_index is the position in the tree for the recursive traversal
+        output:
+            - order implied by the hierarchical tree Z
+
+        seriation computes the order implied by a hierarchical tree (dendrogram)
+    '''
+    if cur_index < N:
+        return [cur_index]
+    else:
+        left = int(Z[cur_index - N, 0])
+        right = int(Z[cur_index - N, 1])
+        return (seriation(Z, N, left) + seriation(Z, N, right))
+
+
+def compute_serial_matrix(dist_matrix, method="ward"):
+    '''
+        input:
+            - dist_mat is a distance matrix
+            - method = ["ward","single","average","complete"]
+        output:
+            - seriated_dist is the input dist_mat,
+              but with re-ordered rows and columns
+              according to the seriation, i.e. the
+              order implied by the hierarchical tree
+            - res_order is the order implied by
+              the hierarhical tree
+            - res_linkage is the hierarhical tree (dendrogram)
+
+        compute_serial_matrix transforms a distance matrix into
+        a sorted distance matrix according to the order implied
+        by the hierarchical tree (dendrogram)
+    '''
+    N = len(dist_matrix)
+    flat_dist_matrix = squareform(dist_matrix)
+    res_linkage = linkage(flat_dist_matrix, method=method, preserve_input=True)
+    res_order = seriation(res_linkage, N, N + N - 2)
+    seriated_dist = np.zeros((N, N))
+    a, b = np.triu_indices(N, k=1)
+    seriated_dist[a, b] = dist_matrix[[res_order[i] for i in a], [res_order[j] for j in b]]
+    seriated_dist[b, a] = seriated_dist[a, b]
+    return seriated_dist, res_order, res_linkage
 
 
 def main():
@@ -124,6 +172,33 @@ def main():
     data_pca = pca.transform(data_tr.data_clustering)
     print("Explained variance ratios:", pca.explained_variance_ratio_,
           "->", sum(pca.explained_variance_ratio_))
+    print("data_clustering:", data_tr.data_clustering)
+    length = len(data_tr.data_clustering)
+    dist_matrix = squareform(pdist(data_tr.data_clustering))
+    plt.pcolormesh(dist_matrix)
+    plt.colorbar()
+    plt.xlim([0, length])
+    plt.ylim([0, length])
+    plt.show()
+    print("The distance matrix:", dist_matrix)
+    label = KMeans(data_pca)
+    u_labels = np.unique(label)
+    for i in u_labels:
+        plt.scatter(data_pca[label == i, 0], data_pca[label == i, 1], label=i)
+        plt.legend()
+        plt.show()
+    print(len(dist_matrix))
+
+    methods = ["ward", "single", "average", "complete"]
+    for method in methods:
+        print("Method:\t", method)
+        ordered_dist_matrix, res_order, res_linkage = compute_serial_matrix(dist_matrix, method)
+        plt.pcolormesh(ordered_dist_matrix)
+        plt.colorbar()
+        plt.xlim([0, len(dist_matrix)])
+        plt.ylim([0, len(dist_matrix)])
+        plt.show()
+        print(squareform(dist_matrix))
 
     # Execute clustering
     clust = Clustering(data=data_pca)
