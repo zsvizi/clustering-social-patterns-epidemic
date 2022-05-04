1f09c4e2ff70965e77156aaf4c6b43a583fb60b5
Evanskorir <104888028+Evanskorir@users.noreply.github.com>
2022-05-04 19:01:13 +0200

    -created and sorted dissimilarity matrix  according to an order imposed by the dendrogram.

--- heatmap.py
+++ heatmap.py
@@ -1,21 +1,20 @@
-import numpy as np
+
 from fastcluster import linkage
 from matplotlib import pyplot as plt
+import numpy as np
+import scipy.cluster.hierarchy as sch
 from scipy.spatial.distance import squareform, pdist
-from sklearn.cluster import KMeans
 
 
 def seriation(Z, N, cur_index):
-    '''
-        input:
-            - Z is a hierarchical tree (dendrogram)
-            - N is the number of points given to the clustering process
-            - cur_index is the position in the tree for the recursive traversal
-        output:
-            - order implied by the hierarchical tree Z
-
-        seriation computes the order implied by a hierarchical tree (dendrogram)
-    '''
+    """
+    It computes the order implied by a hierarchical tree (dendrogram)
+    :{"param_1 Z": a hierarchical tree (dendrogram)
+    "param_2 N": the number of points given to the clustering process
+     "param_3 cur_index": the position in the tree for the recursive traversal
+    }
+    :return: order implied by the hierarchical tree Z
+    """
     if cur_index < N:
         return [cur_index]
     else:
@@ -25,23 +24,18 @@ def seriation(Z, N, cur_index):
 
 
 def compute_serial_matrix(dist_matrix, method="ward"):
-    '''
-        input:
-            - dist_mat is a distance matrix
-            - method = ["ward","single","average","complete"]
-        output:
-            - seriated_dist is the input dist_mat,
-              but with re-ordered rows and columns
-              according to the seriation, i.e. the
-              order implied by the hierarchical tree
-            - res_order is the order implied by
-              the hierarhical tree
-            - res_linkage is the hierarhical tree (dendrogram)
-
-        compute_serial_matrix transforms a distance matrix into
-        a sorted distance matrix according to the order implied
-        by the hierarchical tree (dendrogram)
-    '''
+    """
+    It transforms a distance matrix into a sorted distance matrix according to the order implied by the
+    hierarchical tree (dendrogram)
+    :{"param_1": dist_matrix: input a distance matrix to get a sorted one,
+    "param_2": method: method = ["ward","single","average","complete"]
+    }
+    :{"return_1": seriated_dist: input dist_mat, but with re-ordered rows and columns according to the seriation,
+    i.e. the order implied by the hierarchical tree
+    "return_2": res_order: is the order implied by the hierarchical tree
+    "return_3": res_linkage: is the hierarchical tree (dendrogram)
+    }
+    """
     N = len(dist_matrix)
     flat_dist_matrix = squareform(dist_matrix)
     res_linkage = linkage(flat_dist_matrix, method=method, preserve_input=True)
@@ -54,29 +48,24 @@ def compute_serial_matrix(dist_matrix, method="ward"):
 
 
 def plot_heatmap(data_pca, data_tr):
-    # print("data_clustering:", data_tr.data_clustering)
     length = len(data_tr.data_clustering)
     dist_matrix = squareform(pdist(data_tr.data_clustering))
-    plt.pcolormesh(dist_matrix)
+    plt.pcolormesh(dist_matrix, cmap="rainbow")
+    plt.title("Distance matrix")
     plt.colorbar()
     plt.xlim([0, length])
     plt.ylim([0, length])
     plt.show()
-    # print("The distance matrix:", dist_matrix)
-    label = KMeans(data_pca)
-    u_labels = np.unique(label)
-    for i in u_labels:
-        plt.scatter(data_pca[label == i, 0], data_pca[label == i, 1], label=i)
-        plt.legend()
-        plt.show()
-    # print(len(dist_matrix))
+
     methods = ["ward", "single", "average", "complete"]
     for method in methods:
         print("Method:\t", method)
         ordered_dist_matrix, res_order, res_linkage = compute_serial_matrix(dist_matrix, method)
-        plt.pcolormesh(ordered_dist_matrix)
+        plt.pcolormesh(ordered_dist_matrix, cmap="rainbow")
         plt.colorbar()
         plt.xlim([0, len(dist_matrix)])
         plt.ylim([0, len(dist_matrix)])
+        plt.title(method)
         plt.show()
-        # print(squareform(dist_matrix))
+
+
